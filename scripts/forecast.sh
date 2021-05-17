47e6406f5a18e3546b88bdb658994c86ff064327
Justin Zhang <schnell18@gmail.com>
2021-05-17 08:02:32 +0800

    Escape ampersand

--- scripts/forecast.sh
+++ scripts/forecast.sh
@@ -8,7 +8,7 @@ print_forecast() {
   local format=$(get_tmux_option @forecast-format "%C+%t+%w")
   local city=$(get_tmux_option @forecast-city "")
   local unit=$(get_tmux_option @forecast-temperature-unit "m")
-  echo $(curl http://wttr.in/${city}?${unit}&format=$format)
+  echo $(curl http://wttr.in/${city}?${unit}\&format=$format)
 }
 
 main() {
